8181f1abeaf79452d80b546fd9453af6adb57721
ans_ashkan <ans.ashkan@gmail.com>
2020-04-15 23:18:09 +0430

    optimize prime

--- prime/main.go
+++ prime/main.go
@@ -9,13 +9,32 @@ import (
 	"strconv"
 )
 
+var primes = map[int]bool{}
+
 func isPrimeSqrt(value int) bool {
+
+	if value == 1 {
+		return false
+	} else if value == 2 || value == 3 {
+		return true
+	} else if value%2 == 0 || value%3 == 0 {
+		return false
+	}
+
+	if isPrime, exists := primes[value]; exists {
+		return isPrime
+	}
+
 	for i := 2; i <= int(math.Floor(math.Sqrt(float64(value)))); i++ {
 		if value%i == 0 {
+			primes[value] = false
 			return false
 		}
 	}
-	return value > 1
+
+	isPrime := value > 1
+	primes[value] = isPrime
+	return isPrime
 }
 
 func main() {
